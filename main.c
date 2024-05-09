168f69b96a586e5a629144e07f729cf35b01d9b1
wawaiy <msk.ilyasss@gmail.com>
2024-05-09 18:58:19 +0200

    Ajout de l'index pour gerer les cases en feu cela va nous permettre de vérifier les cases adjacents aux cases en feu au lieu de parcourir l'ensemble de la grille

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+    int x;
+    int y;
+} Coordinate;
+
 int main (){
 
     //Récupération des paramètres du fichier externe et vérification de ceux-ci
@@ -31,12 +36,17 @@ int main (){
     printf("initial_fire_x = %d\n", initial_fire_x);
     printf("initial_fire_y = %d\n", initial_fire_y);
 
-    //création de la grille qu'on remplit avec des 0 dans un premier temps
+    //création de la grille
 
     char **forest = (char **)malloc(length_h * sizeof(char *));
     for (int i = 0; i < length_h; i++) {
         forest[i] = (char *)malloc(length_l * sizeof(char));
     }
+
+    // création d'un tableau d'index ou seront stocker les coordonnées des cases en feu
+    // cela va nous permettre de vérifier les cases adjacents aux cases en feu au lieu de parcourir l'ensemble de la grille
+    Coordinate *fire_index = (Coordinate *)malloc(length_h * length_l * sizeof(Coordinate));
+    int num_fires = 0;
 
     for (int i = 0; i < length_h; i++) {
         for (int j = 0; j < length_l; j++) {
@@ -44,7 +54,13 @@ int main (){
         }
     }
 
+    //pour positionner la première case en feu
+    forest[initial_fire_x][initial_fire_y] = '1';
+    fire_index[num_fires].x = initial_fire_x;
+    fire_index[num_fires].y = initial_fire_y;
+    num_fires++;
 
+    // Affichage de la grille
     printf("\nGrille de la forêt initiale :\n");
     for (int i = 0; i < length_h; i++) {
         for (int j = 0; j < length_l; j++) {
